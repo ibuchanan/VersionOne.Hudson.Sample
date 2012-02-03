7ed634a48874be5cf1b0efd98f6979e2c039e373
Ian Buchanan <ian.buchanan@versionone.com>
2012-02-03 16:01:51 -0500

    basic project structure

---
+++ README.md
@@ -0,0 +1,2 @@
+# Hudson Sample Project
+This project has some Java code that compiles but it doesn't do anything interesting.
---
+++ com.versionone.hudson.sample/src/com/versionone/hudson/sample/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.versionone.hudson.sample;
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
